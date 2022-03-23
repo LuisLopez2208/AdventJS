d6bf370956cebdc1dfe8d6beb24047b1bb64bfb6
LuisLopez <luislopez2208@hotmail.com>
2022-03-23 13:21:13 -0600

    correcion a solucion 2

--- 02/solution.js
+++ 02/solution.js
@@ -1,6 +1,4 @@
-const letter = l => l.split(" ").reduce( (acc, e ) => {
-    if (e != "" && ![...e].includes("_")) {
-        acc[e] = typeof acc[e] != "undefined" ? acc[e] + 1 : 1
-    }
+const letter = l => l.split(" ").reduce( (acc, e ) => {    
+    ( e != "" && ![...e].includes("_") )  && ( acc[e] = typeof acc[e] != "undefined" ? acc[e] + 1 : 1 )    
     return acc
   }, {})
